7de910ea0f4209e1d47fe62679bab616d5d3e373
mbCeli <munbaporicelina@outlook.com>
2025-01-15 14:23:52 +0000

    Solved lab

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const s3 = "Ted";
 const s4 = "bread";
 const s5 = "and";
 
+let tongueTwister = s1 + " " + s2 + " " + s3 + " " + s4 + " " + s5 + " " + s3 + " " + s2 + " " + s1 + " " + s4;
+
+console.log(tongueTwister);
+
 // Concatenate the string variables into one new string
 
 
@@ -21,6 +25,12 @@ const s5 = "and";
 const part1 = "java";
 const part2 = "script";
 
+let result = part1 + part2;
+result = part1.slice(0, -1) + part1.slice(-1).toUpperCase() + part2.slice(0, -1) + part2.slice(-1).toUpperCase();
+
+console.log(result);
+
+
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
 
 
@@ -34,6 +44,10 @@ const part2 = "script";
 *******************************************/
 const billTotal = 84;
 
+let tipAmount = billTotal * 0.15;
+
+console.log(tipAmount);
+
 // Calculate the tip (15% of the bill total)
 
 
@@ -45,6 +59,10 @@ const billTotal = 84;
 /*******************************************
     Iteration 2.2 | Generate Random Number
 *******************************************/
+
+let randomNumber = Math.round(Math.random() * 10 + 1);
+
+console.log(randomNumber);
 
 // Generate a random integer between 1 and 10 (inclusive)
 
@@ -61,16 +79,24 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b; // false
 
-const expression2 = a || b;
+const expression2 = a || b; // true
 
-const expression3 = !a && b;
+const expression3 = !a && b; // true - wrong
 
-const expression4 = !(a && b);
+const expression4 = !(a && b); // true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b; // true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b); // false
 
-const expression7 = a && a;
+const expression7 = a && a; // true
+
+console.log(expression1)
+console.log(expression2)
+console.log(expression3)
+console.log(expression4)
+console.log(expression5)
+console.log(expression6)
+console.log(expression7)
